fe94c18d957df3ccea77250d97622e6f9365c2d6
kim <kjw031015@gmail.com>
2025-06-05 16:29:10 +0900

    calc.cpp 작성 add.cpp 추가 작성

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,11 +1,22 @@
-﻿// calc.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-
+﻿#include <iostream>
+#include "add.h"
+#include "dive.h"
+#include "mod.h"
+#include "mul.h"
+#include "sub.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x = 0;
+    int y = 0;
+
+
+    std::cout << "첫번째 수를 입력하세요 : \n";
+    std::cin >> x;
+    std::cout << "두번째 수를 입력하세요 : \n";
+    std::cin >> y;
+
+    printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
+    printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n", add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
